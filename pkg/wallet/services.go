022c6a2d05d0f12b22d9f36876fc450f6146d2ca
Sher-mad <80169323+Sher-mad@users.noreply.github.com>
2021-06-06 03:41:03 +0500

    Саздал метод Reject & Pay & FindPaymentByID

--- pkg/wallet/services.go
+++ pkg/wallet/services.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	"github.com/Sher-mad/wallet/pkg/types"
+	"github.com/google/uuid"
 )
 
 type Service struct {
@@ -16,6 +17,8 @@ var (
 	ErrPhoneRegistered      = errors.New("phone already registered")
 	ErrAmountMustBePositive = errors.New("amount must be greater than zero")
 	ErrAccountNotFound      = errors.New("account not found")
+	ErrNotEnoughBalance     = errors.New("not enough balance")
+	ErrPaymentNotFound      = errors.New("payment not found")
 )
 
 func (s *Service) RegisterAccount(phone types.Phone) (*types.Account, error) {
@@ -62,4 +65,66 @@ func (s *Service) Deposit(accountID int64, amount types.Money) error {
 
 	account.Balance += amount
 	return nil
+}
+
+func (s *Service) Pay(accountID int64, amount types.Money, category types.PaymentCategory) (*types.Payment, error) {
+	if amount <= 0 {
+		return nil, ErrAmountMustBePositive
+	}
+
+	var account *types.Account
+	for _, acc := range s.accounts {
+		if acc.ID == accountID {
+			account = acc
+			break
+		}
+	}
+	if account == nil {
+		return nil, ErrAccountNotFound
+	}
+
+	if account.Balance < amount {
+		return nil, ErrNotEnoughBalance
+	}
+
+	account.Balance -= amount
+	paymentID := uuid.New().String()
+	payment := &types.Payment{
+		ID:        paymentID,
+		AccountID: accountID,
+		Amount:    amount,
+		Category:  category,
+		Status:    types.PaymentStatusInProgress,
+	}
+	s.payments = append(s.payments, payment)
+	return payment, nil
+}
+
+func (s *Service) FindPaymentByID(paymentID string) (*types.Payment, error) {
+	for _, payment := range s.payments {
+		if payment.ID == paymentID {
+			return payment, nil
+		}
+	}
+	return nil, ErrPaymentNotFound
+}
+
+func (s *Service) Reject(paymentID string) error {
+	payment, err := s.FindPaymentByID(paymentID)
+	if err != nil {
+		return err
+	}
+
+	account, err := s.FindAccountByID(payment.AccountID)
+
+	if err != nil {
+		return err
+	}
+
+	payment.Status = types.PaymentStatusFail
+
+	account.Balance += payment.Amount
+
+	return nil
+
 }
--- pkg/wallet/services_test.go
+++ pkg/wallet/services_test.go
@@ -111,3 +111,109 @@ func TestService_FindAccountByID_fail(t *testing.T) {
 		return
 	}
 }
+
+func TestService_Reject_success(t *testing.T) {
+	svc := &Service{}
+	account, err := svc.RegisterAccount(phone_1)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	account, err = svc.RegisterAccount(phone_2)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	account, err = svc.RegisterAccount(phone_3)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	account, err = svc.RegisterAccount(phone_4)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	account, err = svc.FindAccountByID(1)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	err = svc.Deposit(account.ID, 1000_00)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	payment, err := svc.Pay(account.ID, 800_00, "iPhone 4")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	payment, err = svc.FindPaymentByID(payment.ID)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	err = svc.Reject(payment.ID)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+}
+func TestService_Reject_fail(t *testing.T) {
+	svc := &Service{}
+	_, err := svc.RegisterAccount(phone_1)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_, err = svc.RegisterAccount(phone_2)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_, err = svc.RegisterAccount(phone_3)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_, err = svc.RegisterAccount(phone_4)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	_, err = svc.RegisterAccount(phone_4)
+	if err == nil {
+		t.Error(err)
+		return
+	}
+	_, err = svc.FindAccountByID(int64(uuid.New().ID()))
+	if err == nil {
+		t.Error(err)
+		return
+	}
+
+	err = svc.Deposit(int64(uuid.New().ID()), 1000_00)
+	if err == nil {
+		t.Error(err)
+		return
+	}
+	_, err = svc.Pay(int64(uuid.New().ID()), 800_00, "iPhone 4")
+	if err == nil {
+		t.Error(err)
+		return
+	}
+	_, err = svc.FindPaymentByID(uuid.New().String())
+	if err == nil {
+		t.Error(err)
+		return
+	}
+	err = svc.Reject(uuid.New().String())
+	if err == nil {
+		t.Error(err)
+		return
+	}
+
+}
